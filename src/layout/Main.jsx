802c7e438bc2f1a5e78f3e2de20505c1b09c0eba
Kirhaz <johndoe@example.com>
2021-08-26 18:26:19 +0300

    http to https

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -11,14 +11,14 @@ class Main extends React.Component {
 	}
 
 	componentDidMount() {
-		fetch(`http://www.omdbapi.com/?apikey=b36e2df9&s=matrix`)
+		fetch(`https://www.omdbapi.com/?apikey=b36e2df9&s=matrix`)
 			.then(response => response.json())
 			.then(data => this.setState({ movies: data.Search, loading: false  }))
 	}
 
 	searchMovies = (str, type='all') => {
 		this.setState({loading:true})
-		fetch(`http://www.omdbapi.com/?apikey=b36e2df9&s=${str}${type !== 'all' ? `&type=${type}` : ''}`)
+		fetch(`https://www.omdbapi.com/?apikey=b36e2df9&s=${str}${type !== 'all' ? `&type=${type}` : ''}`)
 			.then(response => response.json())
 			.then(data => this.setState({ movies: data.Search, loading: false }))
 	}
